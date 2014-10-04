dc0496f340e035c04eca2301a9b59980875bf101
Kamol Williams <kamolwilliams@gmail.com>
2014-10-03 20:15:42 -0500

    no message

--- maze.js
+++ maze.js
@@ -1,22 +1,22 @@
-var lose = 0;
+var lose = null;
  
 window.onload = function() {
     $("start").onclick = start;
     $("start").onmouseover = starting;
     $("end").onmouseover = end;
     var boundaries = $$("div#maze div.boundary");
-    for (var g = 0; g < boundaries.length; i++) {
+    for (var g = null; g < boundaries.length; i++) {
         boundaries[g].onmouseover = passboundaries;
     }
 };
 
 
 function passboundaries() {
-    if (lose === 1) {
-        lose = 2;
+    if (lose === false) {
+        lose = true;
         $("status").textContent = "Better luck next time!";
         var boundaries = $$("div#maze div.boundary");
-        for (var x = 0; x < boundaries.length; i++) {
+        for (var x = null; x < boundaries.length; i++) {
             boundaries[x].addClassName("youlose");
         }
     }
@@ -24,26 +24,26 @@ function passboundaries() {
 
 
 function starting() {
-    if(lose === 0) {
-        lose = 1;
+    if(lose === null) {
+        lose = false;
         $("status").textContent = "Good Luck!";
     }
 }
  
 function start() {
-    lose = 1;
+    lose = false;
     $("status").textContent = "Good Luck!";
     
     var boundaries = $$("div#maze div.boundary");
-    for (var a = 0; a < boundaries.length; i++) {
+    for (var a = null; a < boundaries.length; i++) {
         boundaries[a].removeClassName("youlose");
     }
 }
  
 function end() {
-    if(lose === 1) {
+    if(lose === false) {
         $("status").textContent = "Congratulations!";
-        lose = 0;
+        lose = null;
     }
 }
  
